62c960fd7838269ad76a7e28230298b101f68c55
cloudhub7 <signupmain4636@gmail.com>
2025-08-31 23:32:54 -0400

    Update main.lua

--- tabs/main.lua
+++ tabs/main.lua
@@ -1,6 +1,27 @@
+-- tabs/main.lua
+
+-- Make sure 'Window' exists (passed from main.lua)
+-- Create the main tab
 local Tab = Window:CreateTab({
-    Name = "makn",
+    Name = "main",
     Icon = "view_in_ar",
     ImageSource = "Material",
-    ShowTitle = true -- This will determine whether the big header text in the tab will show
+    ShowTitle = true
+})
+
+-- Example button inside the tab
+Tab:CreateButton({
+    Name = "Example Button",
+    Callback = function()
+        print("Button clicked in main tab!")
+    end
+})
+
+-- Example toggle inside the tab
+Tab:CreateToggle({
+    Name = "Example Toggle",
+    Default = false,
+    Callback = function(state)
+        print("Toggle state:", state)
+    end
 })
