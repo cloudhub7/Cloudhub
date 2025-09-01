a64e0a1f02a3fbfc7de5bc27314c760f7836ecd5
cloudhub7 <signupmain4636@gmail.com>
2025-08-31 23:22:20 -0400

    Update main.lua

--- tabs/main.lua
+++ tabs/main.lua
@@ -1,1 +1,6 @@
-
+local Tab = Window:CreateTab({
+    Name = "makn",
+    Icon = "view_in_ar",
+    ImageSource = "Material",
+    ShowTitle = true -- This will determine whether the big header text in the tab will show
+})
