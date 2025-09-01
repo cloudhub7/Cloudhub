c0196728e5a238a8c2630539cf1055f438399e78
cloudhub7 <signupmain4636@gmail.com>
2025-08-31 23:35:35 -0400

    Update main.lua

--- tabs/main.lua
+++ tabs/main.lua
@@ -1,27 +1,28 @@
 -- tabs/main.lua
 
--- Make sure 'Window' exists (passed from main.lua)
--- Create the main tab
-local Tab = Window:CreateTab({
-    Name = "main",
-    Icon = "view_in_ar",
-    ImageSource = "Material",
-    ShowTitle = true
-})
+-- Return a function that takes the Window object
+return function(Window)
+    local Tab = Window:CreateTab({
+        Name = "main",
+        Icon = "view_in_ar",
+        ImageSource = "Material",
+        ShowTitle = true
+    })
 
--- Example button inside the tab
-Tab:CreateButton({
-    Name = "Example Button",
-    Callback = function()
-        print("Button clicked in main tab!")
-    end
-})
+    -- Example button
+    Tab:CreateButton({
+        Name = "Example Button",
+        Callback = function()
+            print("Button clicked in main tab!")
+        end
+    })
 
--- Example toggle inside the tab
-Tab:CreateToggle({
-    Name = "Example Toggle",
-    Default = false,
-    Callback = function(state)
-        print("Toggle state:", state)
-    end
-})
+    -- Example toggle
+    Tab:CreateToggle({
+        Name = "Example Toggle",
+        Default = false,
+        Callback = function(state)
+            print("Toggle state:", state)
+        end
+    })
+end
