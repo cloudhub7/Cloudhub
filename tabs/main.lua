f0627961300688af7d12314af58829ff4440ae77
cloudhub7 <signupmain4636@gmail.com>
2025-09-01 13:53:50 -0400

    Update main.lua

--- tabs/main.lua
+++ tabs/main.lua
@@ -23,4 +23,15 @@ return function(Window)
             print("Main tab toggle state:", state)
         end
     })
+
+    -- Minimize button
+    local guiVisible = true
+    Tab:CreateButton({
+        Name = "Minimize GUI",
+        Callback = function()
+            guiVisible = not guiVisible
+            Window:SetVisibility(guiVisible) -- Hide/Show the window
+            print("GUI visibility:", guiVisible)
+        end
+    })
 end
