8fe1f4c42eabdf8e5553127f73880b5320ff8920
cloudhub7 <signupmain4636@gmail.com>
2025-09-01 12:12:30 -0400

    Update Setting.lua

--- tabs/Setting.lua
+++ tabs/Setting.lua
@@ -1,1 +1,29 @@
+-- tabs/settings.lua
 
+return function(Window)
+    local Tab = Window:CreateTab({
+        Name = "Settings",
+        Icon = "settings",
+        ImageSource = "Material",
+        ShowTitle = true
+    })
+
+    -- Example slider
+    Tab:CreateSlider({
+        Name = "Volume",
+        Min = 0,
+        Max = 100,
+        Default = 50,
+        Callback = function(value)
+            print("Settings tab slider value:", value)
+        end
+    })
+
+    -- Example button
+    Tab:CreateButton({
+        Name = "Reset Settings",
+        Callback = function()
+            print("Settings tab: Reset clicked")
+        end
+    })
+end
